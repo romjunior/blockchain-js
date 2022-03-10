c128ab7c3c25e2e5995ed1cf93057c9d1c667499
Romualdo Junior <romualdo.jrr@gmail.com>
2022-03-10 00:08:16 -0300

    criando testes do walletcontroller

--- blockchain-back/src/controllers/WalletController.test.ts
+++ blockchain-back/src/controllers/WalletController.test.ts
@@ -1,9 +1,11 @@
 import supertest from "supertest";
 import app from "../app";
-import { createWallet } from "../core/Wallets";
+import { createWallet, findPublicKeyByAlias, listAllWallets } from "../core/Wallets";
 
 jest.mock('../core/Wallets');
 const mockCreateWallet = createWallet as jest.MockedFunction<typeof createWallet>;
+const mockFindPublicKeyByAlias = findPublicKeyByAlias as jest.MockedFunction<typeof findPublicKeyByAlias>
+const mockListAllWallets = listAllWallets as jest.MockedFunction<typeof listAllWallets>
 
 const request = supertest(app);
 
@@ -18,12 +20,12 @@ describe('WalletController', () => {
         mockCreateWallet.mockResolvedValue(['private', 'public']);
 
         request.get('/wallet/create?alias=aa')
-        .expect(201, {
-            alias: 'aa',
-            publicKey: 'public',
-            privateKey: 'private',
-            message: 'The private key is yours! you gonna use to manage your wallet, so dont lose it! we not gonna stored here!'
-        }, done);
+            .expect(201, {
+                alias: 'aa',
+                publicKey: 'public',
+                privateKey: 'private',
+                message: 'The private key is yours! you gonna use to manage your wallet, so dont lose it! we not gonna stored here!'
+            }, done);
     });
 
     it('error 500 when creating a wallet', (done) => {
@@ -32,21 +34,114 @@ describe('WalletController', () => {
         });
 
         request.get('/wallet/create?alias=bb')
-        .expect(500, {
-            message: 'error'
-        },done);
+            .expect(500, {
+                message: 'error'
+            }, done);
     });
 
-    it('error 400 when trying to call without alias param', (done) => {
+    it('createWallet:error 400 when trying to call without alias param', (done) => {
         request.get('/wallet/create')
-        .expect(400, {
-            data: [
-                {
-                    msg: 'Invalid value',
-                    param: 'alias',
-                    location: 'query'
+            .expect(400, {
+                data: [
+                    {
+                        msg: 'Invalid value',
+                        param: 'alias',
+                        location: 'query'
+                    }
+                ]
+            }, done);
+    });
+
+    it('find public key with alias with success', (done) => {
+        mockFindPublicKeyByAlias.mockResolvedValue('publickey');
+
+        request.get('/wallet?alias=aaa')
+            .expect(200, {
+                alias: 'aaa',
+                publicKey: 'publickey'
+            }, done);
+    });
+
+    it('findWalletByAlias:error 400 when trying to call without alias param', (done) => {
+        request.get('/wallet')
+            .expect(400, {
+                data: [
+                    {
+                        msg: 'Invalid value',
+                        param: 'alias',
+                        location: 'query'
+                    }
+                ]
+            }, done);
+    });
+
+    it('error 404 when findWalletByAlias not find a wallet', (done) => {
+        mockFindPublicKeyByAlias.mockResolvedValue(undefined);
+
+        request.get('/wallet?alias=aaa')
+            .expect(404, {
+                data: {
+                    message: 'Public key for alias not found'
                 }
-            ]
+            }, done);
+    })
+
+    it('error 500 when findWalletByAlias', (done) => {
+
+        mockFindPublicKeyByAlias.mockImplementation(() => {
+            throw new Error('error');
+        });
+
+        request.get('/wallet?alias=aaa')
+            .expect(500, {
+                message: 'error'
+            }, done);
+    });
+
+    it('list all wallets with success', (done) => {
+        mockListAllWallets.mockResolvedValue([{
+            alias: 'aaa',
+            publicKey: 'publickey'
+        },
+        {
+            alias: 'bbb',
+            publicKey: 'publickey2'
+        }]);
+
+        request.get('/wallet/all')
+            .expect(200, {
+                total: 2,
+                wallets: [
+                    {
+                        alias: 'aaa',
+                        publicKey: 'publickey'
+                    },
+                    {
+                        alias: 'bbb',
+                        publicKey: 'publickey2'
+                    }
+                ]
+            }, done);
+    });
+
+    it('list all wallets with empty wallets list', (done) => {
+        mockListAllWallets.mockResolvedValue([]);
+
+        request.get('/wallet/all')
+            .expect(200, {
+                total: 0,
+                wallets: []
+            }, done);
+    });
+
+    it('error 500 when tries to list all wallets', (done) => {
+        mockListAllWallets.mockImplementation(() => {
+            throw new Error('error');
+        });
+
+        request.get('/wallet/all')
+        .expect(500, {
+            message: 'error'
         }, done);
     });
 });
--- blockchain-back/src/controllers/WalletController.ts
+++ blockchain-back/src/controllers/WalletController.ts
@@ -19,7 +19,7 @@ export const createWalletController: RequestHandler = async (req, res, next) => 
             publicKey,
             message: 'The private key is yours! you gonna use to manage your wallet, so dont lose it! we not gonna stored here!'
         });
-    }catch(err) {
+    } catch (err) {
         return next(err);
     }
 }
@@ -27,25 +27,34 @@ export const createWalletController: RequestHandler = async (req, res, next) => 
 export const findPublicKeyByAliasController: RequestHandler = async (req, res, next) => {
     log.info(`Init request`);
     const alias = req.query.alias as string;
-    const publicKey = await findPublicKeyByAlias(alias);
 
-    if (!publicKey) {
-        log.error(`keys not found`);
-        return next(new NotFoundRequestError({ message: 'Public key for alias not found' }, ''));
+    try {
+        const publicKey = await findPublicKeyByAlias(alias);
+
+        if (!publicKey) {
+            log.error(`keys not found`);
+            return next(new NotFoundRequestError({ message: 'Public key for alias not found' }, ''));
+        }
+        log.info(`success alias=${alias}, publicKey=${publicKey}`);
+        res.status(200).json({
+            alias,
+            publicKey
+        });
+    } catch (e) {
+        next(e);
     }
-    log.info(`success alias=${alias}, publicKey=${publicKey}`);
-    res.status(200).json({
-        alias,
-        publicKey
-    });
 }
 
-export const findAllWalletsController: RequestHandler = async (_req, res) => {
+export const findAllWalletsController: RequestHandler = async (_req, res, next) => {
     log.info(`Init request`);
-    const wallets = await listAllWallets();
-    log.info(`success listAllWallets`);
-    res.status(200).json({
-        total: wallets.length,
-        wallets
-    });
+    try {
+        const wallets = await listAllWallets();
+        log.info(`success listAllWallets`);
+        res.status(200).json({
+            total: wallets.length,
+            wallets
+        });
+    } catch (e) {
+        next(e);
+    }
 }
